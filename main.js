fcb4f7a7e42fd40373d7b9f89db976325594ab5a
Michael Herron <mherron2021@gmail.com>
2021-01-29 19:19:50 +0000

    _

--- main.js
+++ main.js
@@ -48,35 +48,27 @@ function CheckGameOver() {
 
   if (m[0] != null && m[0] === m[1] && m[0] === m[2]) {
     m[0] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[3] != null && m[3] === m[4] && m[3] === m[5]) {
     m[3] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[6] != null && m[6] === m[7] && m[6] === m[8]) {
     m[6] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[0] != null && m[0] === m[3] && m[0] === m[6]) {
     m[0] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[1] != null && m[1] === m[4] && m[1] === m[7]) {
     m[1] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[2] != null && m[2] === m[5] && m[2] === m[8]) {
     m[2] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[0] != null && m[0] === m[4] && m[0] === m[8]) {
     m[0] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   if (m[2] != null && m[2] === m[4] && m[2] === m[6]) {
     m[2] === "X" ? announceWinner(player1.name) : announceWinner(player2.name);
-    showImage();
   }
   // if no null elements remain, and there has been no winner, announce draw.
   if (!m.includes(null) && !document.querySelector("#result").textContent) {
@@ -94,7 +86,6 @@ function reset() {
   gameBoard.displayGame();
   gameWinner.textContent = "";
   currentMarker = "O";
-  document.querySelector("#wellDoneImage").style.display = "none";
 }
 
 function createPlayers() {
